5394bb8f0248e917063839acf80e9a6dd158198d
Nikita Fomichev <nickita.fomichev2016@yandex.ru>
2023-09-19 19:07:40 +0300

    change styles, added lazy loading

--- src/components/NextJsImage/index.tsx
+++ src/components/NextJsImage/index.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import type { RenderPhotoProps } from 'react-photo-album';
-import { motion } from 'framer-motion';
-import localFont from 'next/font/local';
-
-import styles from './styles.module.css';
 import { useState } from 'react';
 import clsx from 'clsx';
+import { motion } from 'framer-motion';
+import localFont from 'next/font/local';
+import { useInView } from 'react-intersection-observer';
+
+import styles from './styles.module.css';
 
 const steppe = localFont({
   src: '../../../public/fonts/Steppe/Steppe.otf',
@@ -26,27 +27,40 @@ const imageVariants = {
 
 export default function NextJsImage({
   photo,
-  imageProps: { alt },
+  imageProps: { alt, onClick },
   wrapperStyle,
 }: RenderPhotoProps) {
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
+
   const [onImage, setMouse] = useState(false);
-  console.log(photo);
   return (
     <motion.div
+      ref={ref}
       style={{ ...wrapperStyle, position: 'relative' }}
       className={styles.imageOverlay}
-      variants={imageVariants}
       initial="hidden"
-      animate="visible"
+      animate={inView ? 'visible' : 'hidden'}
       onMouseEnter={() => {
         setMouse(true);
       }}
       onMouseLeave={() => {
         setMouse(false);
       }}
+      variants={imageVariants}
     >
-      <Image fill src={photo} quality={75} {...{ alt }} />
-      <motion.p
+      {inView ? (
+        <Image
+          fill
+          sizes="75vw"
+          src={photo}
+          quality={70}
+          {...{ alt, onClick }}
+        />
+      ) : null}
+      <p
         className={
           onImage
             ? clsx(styles.description, steppe.className)
@@ -54,7 +68,7 @@ export default function NextJsImage({
         }
       >
         {alt}
-      </motion.p>
+      </p>
     </motion.div>
   );
 }
