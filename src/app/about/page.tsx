a0d0276e0a557d340bb1fe7e4bbb3dea4c008f58
Nikita Fomichev <nickita.fomichev2016@yandex.ru>
2023-09-28 23:54:20 +0300

    added page 404

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,10 @@
 import { Metadata } from 'next';
+import Image from 'next/image';
 
 import styles from './styles.module.css';
+
+import photo1 from '../../../public/images/photo1.jpg';
+import photo2 from '../../../public/images/photo2.jpg';
 
 export const metadata: Metadata = {
   title: 'About | Duende',
@@ -12,6 +16,15 @@ export default function About() {
       <div className={styles.headingWrapper}>
         <h2 className={styles.heading}>Hey there! We&nbsp;are Duende.</h2>
       </div>
+      <div className={styles.photoWrapper1}>
+        <Image
+          src={photo2}
+          width={300}
+          height={300}
+          alt="our team"
+          className={styles.photo}
+        />
+      </div>
       <div className={styles.aboutWrapper}>
         <p className={styles.text}>
           Duende&nbsp;&mdash; is&nbsp;a&nbsp;small but very hardworking and
@@ -22,6 +35,15 @@ export default function About() {
           It&nbsp;is&nbsp;important for&nbsp;us to&nbsp;make each image special,
           attractive, and unique, leaving a&nbsp;pleasant aftertaste.
         </p>
+      </div>
+      <div className={styles.photoWrapper2}>
+        <Image
+          src={photo1}
+          width={300}
+          height={300}
+          alt="our team"
+          className={styles.photo}
+        />
       </div>
     </div>
   );
